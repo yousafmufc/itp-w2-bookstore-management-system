47e6fcd13f05aaa463d34cee0bac9efea695b97f
Santiago Basulto <santiago.basulto@gmail.com>
2018-02-21 19:14:12 -0300

    Solution

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,51 @@
 def create_bookstore(name):
-    pass
+    return {
+        'name': name,
+        'books': [],
+        'authors': []
+    }
 
 
 def get_bookstore_name(bookstore):
-    pass
+    return bookstore['name']
 
 
 def add_author(bookstore, name, nationality):
-    pass
+    author = {
+        'name': name,
+        'nationality': nationality
+    }
+    bookstore['authors'].append(author)
+
+    return author
 
 
 def get_author_by_name(bookstore, name):
-    pass
+    for author in bookstore['authors']:
+        if author['name'] == name:
+            return author
 
 
 def add_book(bookstore, title, isbn, author):
-    pass
+    book = {
+        'title': title,
+        'isbn': isbn,
+        'author': author
+    }
+    bookstore['books'].append(book)
+
+    return book
 
 
 def get_book_by_title(bookstore, title):
-    pass
-
+    for book in bookstore['books']:
+        if book['title'] == title:
+            return book
 
 def get_books_by_author(bookstore, author):
-    pass
+    books = []
+    for book in bookstore['books']:
+        if book['author'] == author:
+            books.append(book)
+
+    return books
