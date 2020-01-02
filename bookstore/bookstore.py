44035bbf5127f2e2d88dfa8fa4544d1db45b1514
yousafmufc <yousafmufc@gmail.com>
2020-01-01 22:03:05 -0500

    Completed bookstore project

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,66 @@
 def create_bookstore(name):
-    pass
+    return {
+      'name': name,
+      'authors': [],
+      'books': [],
+    }
 
 
 def get_bookstore_name(bookstore):
-    pass
+    
+    return bookstore['name']
 
 
 def add_author(bookstore, name, nationality):
-    pass
+    
+    author_dict = {
+                    'name': name,
+                    'nationality': nationality
+                  }
+   
+    bookstore['authors'].append(author_dict)
+    
+    return author_dict
 
 
 def get_author_by_name(bookstore, name):
-    pass
+    
+    
+    for author in bookstore['authors']:
+      if author['name'] == name:
+        return author
+        
 
 
 def add_book(bookstore, title, isbn, author):
-    pass
+    
+    book = {
+            'title': title,
+            'isbn': isbn,
+            'author': author,
+           }
+    bookstore['books'].append(book)
+    
+    return book
+    
 
 
 def get_book_by_title(bookstore, title):
-    pass
+    
+    for book in bookstore['books']:
+      if book['title'] == title:
+        return book
+                    
 
 
 def get_books_by_author(bookstore, author):
-    pass
+  books = []
+  
+  for book in bookstore['books']:
+    if book['author'] == author:
+      books.append(book)
+      
+  return books
+  
+  
+    
